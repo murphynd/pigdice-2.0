e84bfeb1f1fc5c1ff799296368a10c95750153f1
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-09-01 11:41:58 -0700

    all tests have run

--- _tests_/triangle.test.js
+++ _tests_/triangle.test.js
@@ -7,4 +7,32 @@ describe("Triangle", () => {
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
   });
+  test("should correctly determine whether three lengths are not a triangle", () => {
+    const notTriangle = new Triangle(3, 9, 22);
+    expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
+  test("should correctly determine whether three lengths make a scalene triangle", function () {
+    const scalTriangle = new Triangle(4, 5, 7);
+    expect(scalTriangle.checkType()).toEqual("scalene triangle");
+  });
+  test("should correctly determine whether three lengths make an isosceles triangle", () => {
+    const isoTriangle = new Triangle(5, 5, 7);
+    expect(isoTriangle.checkType()).toEqual("isosceles triangle");
+  });
+  test("should correctly determine wheather three lengths make an equilateral tirangle", () => {
+    const equiTriangle = new Triangle(5, 5, 5);
+    expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  });
+});
+
+describe("Triangle", () => {
+  let reusableTriangle;
+
+  beforeEach(() => {
+    reusableTriangle = new Triangle(5, 5, 5);
+  });
+
+  test("should show how beforeEach() works", () => {
+    console.log(reusableTriangle);
+  });
 });
